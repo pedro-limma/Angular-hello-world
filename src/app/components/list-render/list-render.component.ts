afb44a854bd0ad7e92f66f048964947c889e4274
Pedro Augusto Pinto de Lima <pedro.plima@csu.com.br>
2023-10-17 23:38:41 -0300

    tipando dados pra animal utilizando interfaces

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Interfaces/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
+
 export class ListRenderComponent {
-  animals = [
-    { name: "Chuck", type: "Dog" },
-    { name: "Tiffany", type: "Dog" },
-    { name: "Natasha", type: "Cat" },
-    { name: "Nina", type: "Cat" }
+  animals: Animal[] = [
+    { name: "Chuck", type: "Dog", age: 0.7 },
+    { name: "Tiffany", type: "Dog", age: 1 },
+    { name: "Natasha", type: "Cat", age: 4 },
+    { name: "Nina", type: "Cat", age: 3 }
   ]
+
+  animalDetails = "";
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
+  }
 }
